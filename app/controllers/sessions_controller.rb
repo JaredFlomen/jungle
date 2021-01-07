7e4be1c27b30a5d4516122e550c21e4145d76800
Jared <jaredflomen@gmail.com>
2021-01-07 16:10:23 +0000

    Add logic to login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    #If the user exisits and the password entered is correct
+    if user && user.authenticate(params[:password])
+      #Save id to cookie
+      session[:user_id] = user.id
+    else
+      #Login fail, redirect to login form
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
   
 end
