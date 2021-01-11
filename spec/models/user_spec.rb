08f6b0644d1a5d9924706c99622b7e7e7bdc618c
Jared <jaredflomen@gmail.com>
2021-01-11 20:13:17 +0000

    Testing for unique emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,16 @@ RSpec.describe User, type: :model do
     end
 
     it 'Passwords must be present' do
-      @user = User.new((password: '123', password_confirmation: '123')
-      expect(@user).to be_valid)
+      @user = User.new(password: '123', password_confirmation: '123')
+      expect(@user).to be_valid
+    end
+
+    it 'Emails must be unique' do
+      _existing_user = User.create(
+        email: "jared@gmail.com"
+      )
+      @user = User.new(email: 'dafd@gmail.com')
+      expect(@user).to_not be_valid
     end
   end
 end
