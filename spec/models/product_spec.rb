0b382ba6efc2fded5ad026e9b94389624438a9f6
Jared <jaredflomen@gmail.com>
2021-01-11 19:24:02 +0000

    testing when quantity is nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Product, type: :model do
       @product = @category.products.new(name: 'jared', price: 100, quantity: 10)
       expect(@product).to be_valid
     end
+    it 'Tests when quantity is nil' do
+      @category = Category.new(name: "test")
+      @product = @category.products.new(name: 'jared', price: 100, quantity: nil)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
   end
 end
