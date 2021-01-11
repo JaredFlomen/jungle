2c087c8765da4aacc97da578c6e75a1792dce092
Jared <jaredflomen@gmail.com>
2021-01-11 19:20:24 +0000

    initial example to test the product is saved successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'Product will save successfully' do
+      @category = Category.new(name: "test")
+      @product = @category.products.new(name: 'jared', price: 100, quantity: 10)
+      expect(@product).to be_valid
+    end
+  end
 end
